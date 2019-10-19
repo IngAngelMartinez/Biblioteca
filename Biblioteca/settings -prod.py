02f988b8f130a8814b9ea7bed7007632b978a882
Angel <fenixturbo@hotmail.com>
2019-10-19 17:25:12 +0000

    repo actualizado

--- Biblioteca/settings -prod.py
+++ Biblioteca/settings -prod.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AngelMartinez04$Biblioteca',
+	'USER': 'AngelMartinez04',
+	'PASSWORD': 'pythonsql',
+	'HOST': 'AngelMartinez04.mysql.pythonanywhere-services.com'
     }
 }
 
